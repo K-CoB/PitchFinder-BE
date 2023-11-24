998cd37ff105497c4590b5cca2783aef79c01869
chaeri93 <chaeri93@ewhain.net>
2023-11-24 16:26:35 +0900

    init

---
+++ src/main/java/com/CoB/PitchFinder/PitchFinderApplication.java
@@ -0,0 +1,13 @@
+package com.CoB.PitchFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CoB/PitchFinder/PitchFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoB.PitchFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
